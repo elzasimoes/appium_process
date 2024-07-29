a8112bc93ba9b691e873332abbc4d772d6660f83
Elza Simões <elzaesimoes@gmail.com>
2024-07-29 18:46:56 -0400

    Create process.py

---
+++ process.py
@@ -0,0 +1,32 @@
+import psutil
+import subprocess
+import time
+
+def start_appium_server(port, log_file):
+    command = [
+        'appium',
+        '-p', str(port),  # Define a porta para o servidor Appium
+        '--log', log_file # Arquivo de log para saída do Appium
+    ]
+    
+    return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def find_pid_by_port(port):
+    for conn in psutil.net_connections(kind='inet'):
+        if conn.laddr.port == port:
+            pid = conn.pid
+            return pid
+    return None
+
+def close_process(pid):
+    process = psutil.Process(pid)
+    process.terminate()
+    process.wait()
+
+
+port = 4725
+start_appium_server(port, 'appium_{0}.log'.format(port))
+time.sleep(15)
+pid = find_pid_by_port(port)
+close_process(pid)
